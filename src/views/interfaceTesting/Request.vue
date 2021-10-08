1dc2275dc2c13ecb180a5ec9bcc562c16eac40ba
yushwuen1013 <751974071@qq.com>
2021-10-08 19:34:39 +0800

    断言

--- src/views/interfaceTesting/Request.vue
+++ src/views/interfaceTesting/Request.vue
@@ -9,21 +9,21 @@
         </el-select>
       </el-form-item>
       <el-form-item label="请求地址" prop="requestAddress">
-       <el-select v-model="form.environment" clearable placeholder="请选择环境">
-        <el-option
-          v-for="item in environment_options"
-          :key="item.id"
-          :label="item.environment_name"
-          :value="item.id">
-        </el-option>
-      </el-select>
+        <el-select v-model="form.environment" clearable placeholder="请选择环境">
+          <el-option
+            v-for="item in environment_options"
+            :key="item.id"
+            :label="item.environment_name"
+            :value="item.id"
+          ></el-option>
+        </el-select>
         <el-input v-model="form.requestAddress" placeholder="请输入地址" style="width: 500px;"></el-input>
         <el-button style="margin-left: 10px" type="primary" @click="sendRequest('form')">Send</el-button>
         <el-button type="primary" @click="save(form)">Save</el-button>
       </el-form-item>
       <el-tabs
         type="border-card"
-        style="max-height: 350px; overflow: auto;"
+        style="max-height: 320px; overflow: auto;"
         @tab-click="handleClick"
       >
         <!-- 请求头 -->
@@ -39,12 +39,41 @@
           <vue-json-editor v-model="bodyData" :showBtns="false" :mode="'code'" lang="zh" />
         </el-tab-pane>
         <el-tab-pane label="断言" name="assert">
-          <h1>断言</h1>
+          <template >
+            <el-radio-group v-model="assertType" @change='assert'>
+               <el-radio :label="1" border>响应断言</el-radio>
+              <el-radio :label="2" border>Json断言</el-radio>
+            </el-radio-group>
+            <div v-if="showAssert == 0"></div>
+            <div v-if="showAssert == 1" style="margin-top: 20px;margin-left: 10px;">
+              <template>
+                <el-radio v-model="responseAssertRules" :label="1">包含</el-radio>
+                <el-radio v-model="responseAssertRules" :label="2">匹配</el-radio>
+              </template>
+              <el-input
+                style="margin-top: 20px;"
+                type="textarea"
+                :rows="5"
+                placeholder="请输如响应断言内容"
+                v-model="responseAssertContent"
+              ></el-input>
+            </div>
+            <div v-if="showAssert == 2" style="margin-top: 20px;margin-left: 10px;">
+              <el-form label-position="top" label-width="80px" :model="jsonAssertFrom">
+                <el-form-item label="Json路径">
+                  <el-input v-model="jsonAssertFrom.json_path"></el-input>
+                </el-form-item>
+                <el-form-item label="预期值">
+                  <el-input v-model="jsonAssertFrom.json_value"></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+          </template>
         </el-tab-pane>
       </el-tabs>
     </el-form>
-    <el-divider></el-divider>
-    <div style="margin-top: 70px;">
+    <div style="margin-top: 40px;">
+      <el-divider></el-divider>
       <Response />
     </div>
     <el-dialog title="保存" :visible.sync="dialogFormVisible">
@@ -95,7 +124,7 @@ import {
 } from "@/api/interfaceTesting";
 import Response from "./Response";
 import Tables from "./Tables";
-import { createNamespacedHelpers } from 'vuex';
+import { createNamespacedHelpers } from "vuex";
 export default {
   components: { vueJsonEditor, Response, Tables },
   data() {
@@ -109,13 +138,28 @@ export default {
       }
     };
     return {
+      //Json断言表单
+      jsonAssertFrom:{
+        json_path: "",
+        json_value: ""
+      },
+      //根据断言类型显示断言内容
+      showAssert: 0,
+      //响应断言规则
+      responseAssertRules: 1,
+      //响应断言的内容
+      responseAssertContent: "",
+      //断言类型
+      assertType: 0, //1是响应断言，2是Json断言
       //环境选项
       environment_options: [],
       //请求数据状态码 1是params,2是Json
       dataStateCode: 2,
       //:rules: rules表单规则校验
       rules: {
-        requestAddress: [{ required: true, trigger: "blur", validator: notNull }],
+        requestAddress: [
+          { required: true, trigger: "blur", validator: notNull }
+        ],
         request_name: [{ required: true, trigger: "blur", validator: notNull }]
       },
       bodyData: {},
@@ -140,16 +184,16 @@ export default {
         //请求类型
         requestType: "get",
         //环境（域名）
-        environment: '',
+        environment: "",
         //请求Url
-        requestAddress: "http://127.0.0.1:8000/user/login"
+        requestAddress: "/user/login"
       },
       //保存
       dialogFormVisible: false, // 保存表单显隐
       currentPage: 1, //初始页
       pagesize: 5, //    每页的数据
       //保存的表单
-      saveForm: { 
+      saveForm: {
         request_name: null,
         tableData: [],
         //所选择的文件夹
@@ -158,9 +202,17 @@ export default {
     };
   },
   methods: {
+    //判断断言类型切换断言的详情
+    assert(val){
+      if(val == 1){
+        this.showAssert = 1
+      }else{
+        this.showAssert = 2
+      }
+    },
     //获取环境名称
-    get_environment_name(val){
-      console.log(val)
+    get_environment_name(val) {
+      console.log(val);
     },
     //获取row的id
     getRowKeys(row) {
@@ -190,6 +242,17 @@ export default {
               params[elem.key] = elem.value;
             }
           });
+          const assert = {
+                assert_type : this.assertType,
+              }
+          if(assert.assert_type == 1){
+              assert.response_assert_rules = this.responseAssertRules
+              assert.response_assert_content = this.responseAssertContent
+            }else if(assert.assert_type == 2){
+              assert.json_path = this.jsonAssertFrom.json_path
+              assert.json_value = this.jsonAssertFrom.json_value
+            }
+          console.log(assert)
           this.request_data = {
             environment_id: this.form.environment,
             address: this.form.requestAddress,
@@ -197,7 +260,8 @@ export default {
             body: this.bodyData,
             headers: header,
             params: params,
-            dataState: this.dataStateCode
+            dataState: this.dataStateCode,
+            assert: assert
           };
           console.log(this.request_data);
           //发送请求，返回数据
@@ -316,10 +380,10 @@ export default {
       this.saveForm.multipleSelection = val;
     }
   },
-  created(){
-    get_environment_configuration().then(response=>{
-      this.environment_options = response.data
-    })
+  created() {
+    get_environment_configuration().then(response => {
+      this.environment_options = response.data;
+    });
   }
 };
 </script>
--- src/views/interfaceTesting/Response.vue
+++ src/views/interfaceTesting/Response.vue
@@ -1,41 +1,104 @@
 <template>
-    <div>
-      <h3>响应状态码：{{resultInfo.response_code}}</h3>
-      <vue-json-editor
-        v-model="resultInfo.response_data"
-        :showBtns="false"    
-        :mode="'code'"   
-        lang="zh" 
-        />
-    </div>
+  <div style='position: relative;'>
+    <h4>
+      <p style="height: 10px;">Response</p>
+    </h4>
+    <template >
+      <el-tabs style="margin-left: 10px;" v-model="activeName">
+        <el-tab-pane label="Body" name="Body">
+          <template>
+            <el-radio-group v-model="responseDataType" @change='ResponseDataType'>
+              <el-radio  :label="1">Raw</el-radio>
+              <el-radio  :label="2">Json</el-radio>
+            </el-radio-group>
+            <el-input
+            readonly
+            style="margin-top: 10px;"
+            v-if="showResponseDataType"
+            type="textarea"
+            :rows="8"
+            placeholder="响应数据"
+            v-model="response.response_data"
+          ></el-input>
+           <json-viewer
+              v-else
+              style="max-height:200px;overflow:auto;"
+              :value="JSON.parse(response.response_data)"
+              :expand-depth="2"
+              copyable
+              sort>
+            </json-viewer>
+          </template>
+        </el-tab-pane>
+        <el-tab-pane label="Headers" name="Headers">
+           <json-viewer
+              style="max-height:200px;overflow:auto;"
+              :value="response.response_headers"
+              :expand-depth="2"
+              copyable
+              sort>
+            </json-viewer>
+        </el-tab-pane>
+        <el-tab-pane label="断言结果" name="assertionResults">
+           <h5>断言类型：{{response.assertion.assertion_type}}</h5>
+           <h5>断言结果：{{response.assertion.assertion_results}}</h5>
+           <h5>断言内容：{{response.assertion.assertion_content}}</h5>
+        </el-tab-pane>
+      </el-tabs>
+      <h4 style='position: absolute;right:40px;top:25px;'>Status：{{response.response_code}}</h4>
+    </template>
+    
+  </div>
 </template>
 
 <script>
-  import vueJsonEditor from 'vue-json-editor'
-  export default {
-    name:'Response',
-    components: {
-      vueJsonEditor
-    },
-    data () {
-      return {
-        resultInfo: '',
+import JsonViewer from "vue-json-viewer";
+import vueJsonEditor from "vue-json-editor";
+export default {
+  name: "Response",
+  components: {
+    vueJsonEditor,
+    JsonViewer
+  },
+  data() {
+    return {
+      responseDataType: 1,
+      showResponseDataType: true,
+      activeName: "Body",
+      response: {
+        response_code: "",
+        response_data : "{}",
+        response_headers : "{}",
+        assertion: ""
       }
+    };
+  },
+  methods: {
+    //切换响应数据
+    ResponseDataType(val){
+        if(val == 1){
+            this.showResponseDataType = true
+        }else{
+          this.showResponseDataType = false
+        }
     },
-    mounted(){
-      this.$bus.$on('response',(data)=>{
-        console.log('Pesponse，收到了数据',data)
-        this.resultInfo = data
-			})
-    },
-    beforeDestroy() {
-			this.$bus.$off('response')
-		},
+  },
+  mounted() {
+    this.$bus.$on("response", data => {
+      console.log("Pesponse，收到了数据", data);
+      this.response = {
+        response_data : JSON.stringify(data.response_data),
+        response_headers : data.response_headers,
+        response_code: data.response_code,
+        assertion: data.assertion,
+      }
+    });
+  },
+  beforeDestroy() {
+    this.$bus.$off("response");
   }
-    
-
+};
 </script>
 
 <style>
-
 </style>
